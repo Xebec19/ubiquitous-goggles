c507681b93868a58d911acdc079e8eec66a36a81
xebec19 <rohant946@gmail.com>
2023-03-03 08:02:36 +0530

    wip: seach bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,17 +2,20 @@ import { useState } from "react";
 import { Text } from "react-native";
 import { TextInput } from "react-native-paper";
 import styled from "styled-components";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function SearchBar() {
-  const [searchText, setSearchText] = useState("");
+  const SearchWrapper = styled.View`
+    padding: 5px;
+  `;
 
   return (
-    <TextInput
-      label="Search"
-      mode="outlined"
-      value={searchText}
-      onChangeText={(text) => setSearchText(text)}
-      left={<Iconicons name="md-map" />}
-    />
+    <SearchWrapper>
+      <TextInput
+        mode="outlined"
+        placeholder="Seach"
+        left={<Ionicons name="ios-information-circle" />}
+      />
+    </SearchWrapper>
   );
 }
